a50297d2448da141b6e1911d45c2678891eaecff
nekomehako <ngnlkkngsm@yahoo.co.jp>
2021-12-15 23:41:05 +0900

    スタイルを追加

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -9,6 +9,14 @@ window.onload = () => {
   const canvas = <HTMLCanvasElement>document.getElementById("graph");
   const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
 
+  ctx.lineWidth = 2;
+  ctx.lineCap = "round";
+  ctx.lineJoin = "round";
+  ctx.shadowOffsetX = 5;
+  ctx.shadowOffsetY = 5;
+  ctx.shadowColor = "rgb(50, 50, 50)";
+  ctx.shadowBlur = 5;
+
   const g = new Gaussian(1, 0);
   const xs: number[] = [0, 1, 2, 3, 4, 5];
   const ys: number[] = xs.map((e) => g.calc(e)); //[10, 20, 30, 50, 70, 40, 30, 20, 50];
