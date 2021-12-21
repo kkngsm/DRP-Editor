e145fa8f6b148cc9c9ef266ddefcf1547387a657
nekomehako <ngnlkkngsm@yahoo.co.jp>
2021-12-21 15:47:47 +0900

    テクスチャ読み込みを追加

--- src/ts/render/glsl/sss.frag
+++ src/ts/render/glsl/sss.frag
@@ -1,6 +1,9 @@
 in vec2 fragCoord;
 layout (location = 0) out vec4 fragColor;
 
+uniform sampler2D tex;
+uniform float col;
 void main(){
-  fragColor = vec4(fragCoord, 0,1);
+  vec4 col = texture(tex, fragCoord);
+  fragColor = vec4(col.xyz, 1.);
 }
--- src/ts/render/render.ts
+++ src/ts/render/render.ts
@@ -1,4 +1,5 @@
 import {
+  CanvasTexture,
   GLSL3,
   IUniform,
   Mesh,
@@ -24,12 +25,13 @@ export default class Render {
     this._camera.position.z = 100;
     this._scene.add(this._camera);
 
-    // マテリアルを作成
     const plane = new PlaneGeometry(1.0, 1.0);
 
     this._uniforms = {
       screenWidth: <IUniform>{ type: "f", value: canvas.clientWidth },
       screenHeight: <IUniform>{ type: "f", value: canvas.clientHeight },
+      tex: <IUniform>{ type: "t", value: this.texture() },
+      col: <IUniform>{ type: "f", value: 0.5 },
     };
 
     const mat = new ShaderMaterial({
@@ -40,12 +42,24 @@ export default class Render {
     });
 
     const mesh = new Mesh(plane, mat);
-
     this._scene.add(mesh);
   }
   draw() {
     this._renderer.setRenderTarget(null);
     this._renderer.clear();
     this._renderer.render(this._scene, this._camera);
+  }
+  private texture(): CanvasTexture {
+    const canvas = document.createElement("canvas");
+    canvas.width = 500;
+    canvas.height = 250;
+
+    const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(canvas.width * 0.5, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width * 0.5, canvas.height);
+    return new CanvasTexture(canvas);
   }
 }
